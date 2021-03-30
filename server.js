f26ed5c11aedf819197ab769200337b28c6cd0cc
Asher623 <69222787+Asher623@users.noreply.github.com>
2021-03-30 15:58:34 -0400

    asdasfasf

--- server.js
+++ server.js
@@ -78,9 +78,7 @@ app.get("/api/user/favourites",  passport.authenticate('jwt', { session: false }
 })
 
 app.put("/api/user/favourites/:id", passport.authenticate('jwt', { session: false }), (req,res) =>{
-    console.log(req.body + "IDddd");
-    console.log(req.user._id);
-    userService.addFavourite(req.user._id, req.body)
+    userService.addFavourite(req.user._id, req.params.id)
     .then((msg) => {
 
         res.json({ "message": msg });
