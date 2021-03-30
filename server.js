89cce114392ae3857c4783f24019d55f20805854
Asher623 <69222787+Asher623@users.noreply.github.com>
2021-03-30 16:20:45 -0400

    zxc

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.put("/api/user/favourites/:id", passport.authenticate('jwt', { session: fals
 })
 
 app.delete("/api/user/favourites/:id", passport.authenticate('jwt', { session: false }), (req,res) =>{
-    userService.removeFavourite(req.user._id, req.body)
+    userService.removeFavourite(req.user._id, req.params.id)
     .then((msg) => {
 
         res.json({ "message": msg });
